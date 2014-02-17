9c6f819f9d102146f0115c3189549ce78d3c3052
James Halliday <mail@substack.net>
2014-02-16 17:46:57 -0800

    concat and collect stream tests

--- test/stream.js
+++ test/stream.js
@@ -3,6 +3,9 @@ var level = require('level');
 var levelProxy = require('../');
 var tmpdir = require('os').tmpdir();
 
+var through = require('through');
+var through2 = require('through2');
+
 var a = level(tmpdir + '/level-swap-a-' + Math.random(), { encoding: 'json' });
 var b = level(tmpdir + '/level-swap-b-' + Math.random(), { encoding: 'json' });
 
@@ -19,24 +22,21 @@ b.batch([
 ]);
 
 test(function (t) {
-    t.plan(2);
+    t.plan(4);
+    
+    function arows (rows) {
+        t.deepEqual(rows, [
+            { key: 'a', value: 3 },
+            { key: 'b', value: 4 },
+            { key: 'c', value: 5 }
+        ]);
+    }
     
     var db = levelProxy();
-    collect(db.createReadStream(), function (rows) {
-        t.deepEqual(rows, [
-            { key: 'a', value: 3 },
-            { key: 'b', value: 4 },
-            { key: 'c', value: 5 }
-        ]);
-    });
-    
-    collect2(db.createReadStream(), function (rows) {
-        t.deepEqual(rows, [
-            { key: 'a', value: 3 },
-            { key: 'b', value: 4 },
-            { key: 'c', value: 5 }
-        ]);
-    });
+    collect(db.createReadStream(), arows);
+    collect2(db.createReadStream(), arows);
+    db.createReadStream().pipe(concat(arows));
+    db.createReadStream().pipe(concat2(arows));
     
     setTimeout(function () {
         db.swap(a);
@@ -61,4 +61,18 @@ function collect2 (s, cb) {
             rows.push(row);
         }
     }
+}
+
+function concat (cb) {
+    var rows = [];
+    return through(write, end);
+    function write (row) { rows.push(row) }
+    function end () { cb(rows) }
+}
+
+function concat2 (cb) {
+    var rows = [];
+    return through2({ objectMode: true }, write, end);
+    function write (row, enc, next) { rows.push(row); next() }
+    function end () { cb(rows) }
 }
